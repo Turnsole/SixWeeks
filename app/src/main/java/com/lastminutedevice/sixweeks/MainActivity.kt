8f8f03c343a6c68a4842daccbddb4945a36f73bb
Christa Mabee <mail@lastminutedevice.com>
2025-05-30 13:07:04 -0300

    Cleaned up MainActivity.

--- app/src/main/java/com/lastminutedevice/sixweeks/MainActivity.kt
+++ app/src/main/java/com/lastminutedevice/sixweeks/MainActivity.kt
@@ -13,13 +13,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Set up the Views.
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Navigation and Fragment management.
