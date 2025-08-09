a153974a1d9a6455f0c5006c9a87e76f000499d2
Christa Mabee <mail@lastminutedevice.com>
2025-08-09 11:15:24 -0300

    completion indicators

--- app/src/main/java/com/lastminutedevice/sixweeks/ui/program/ProgramAdapter.kt
+++ app/src/main/java/com/lastminutedevice/sixweeks/ui/program/ProgramAdapter.kt
@@ -1,12 +1,14 @@
 package com.lastminutedevice.sixweeks.ui.program
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lastminutedevice.sixweeks.R
 import com.lastminutedevice.sixweeks.data.models.UserWorkout
 import com.lastminutedevice.sixweeks.databinding.ProgramAdapterItemDayBinding
 import com.lastminutedevice.sixweeks.databinding.ProgramAdapterWeekBinding
+import com.lastminutedevice.sixweeks.ui.SetDisplayCalculator
 
 class ProgramAdapter() : RecyclerView.Adapter<ProgramViewHolder>() {
 
@@ -30,7 +32,7 @@ class ProgramAdapter() : RecyclerView.Adapter<ProgramViewHolder>() {
         viewType: Int
     ): ProgramViewHolder {
         return ProgramViewHolder(
-            binding = ProgramAdapterWeekBinding.inflate(LayoutInflater.from(parent.context))
+            binding = ProgramAdapterWeekBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
@@ -45,13 +47,23 @@ class ProgramAdapter() : RecyclerView.Adapter<ProgramViewHolder>() {
         }
 
         holder.container.removeAllViews()
+        val displayCalculator = SetDisplayCalculator(holder.itemView.resources)
         workouts.forEach { workout ->
             val setView = ProgramAdapterItemDayBinding.inflate(LayoutInflater.from(holder.itemView.context))
             setView.day.text = holder.container.context.getString(
                 R.string.program_list_day,
-                workout.day,
-                workout.sets.joinToString(", ")
+                workout.day
             )
+            setView.sets.text = workout.sets.joinToString(", ") {
+                displayCalculator.repString(it)
+            }
+            if (workout.completed != null) {
+                setView.completed.visibility = View.VISIBLE
+                setView.notCompleted.visibility = View.GONE
+            } else {
+                setView.completed.visibility = View.GONE
+                setView.notCompleted.visibility = View.VISIBLE
+            }
             holder.container.addView(setView.root)
         }
     }
